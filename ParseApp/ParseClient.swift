99cfafadbc282010a3b9da2d27b75d5d1728d860
Nicholas McDonald <nmmcdona@ucsd.edu>
2017-02-06 10:19:51 -0800

    Implemented a login. It is not working yet, however.

--- ParseApp/ParseClient.swift
+++ ParseApp/ParseClient.swift
@@ -8,6 +8,59 @@
 
 import Parse
 
+enum ParseClientError: Error {
+    case loginError(String)
+}
+
 class ParseClient: NSObject {
+    
+    static let sharedInstance: ParseClient = ParseClient()
+    static var loggedInUser: PFUser?
+    
+    func newUser(username: String, email: String, password: String) -> PFUser {
+        
+        let user = PFUser()
+        user.username = username
+        user.email = email
+        user.password = password
+        
+        return user
+    }
+    
+    
+    func login(user: PFUser, success: @escaping (PFUser) -> (), failure: @escaping (Error) -> ()) {
+        let task: BFTask<PFUser> = PFUser.logInWithUsername(inBackground: user.username!, password: user.password!)
+        task.continue ({ (theTask: BFTask<PFUser>) -> Any? in
+            if theTask.isCancelled {
+                // The task was cancelled! Do some error thing here.
+                let errorDescription: String = "The login task was cancelled!"
+                failure(ParseClientError.loginError(errorDescription))
+            } else if let error: Error = theTask.error {
+                // There was an error
+                failure(error)
+            } else {
+                // The login was successful.
+                if let nowLoggedInUser: PFUser = theTask.result {
+                    ParseClient.loggedInUser = nowLoggedInUser
+                    success(nowLoggedInUser)
+                } else {
+                    let errorDescription: String = "PFUser that was returned was nil"
+                    failure(ParseClientError.loginError(errorDescription))
+                }
+            }
+            return nil
+        })
+    }
+    
+    func signUp(user: PFUser, success: @escaping () -> (), failure: @escaping (Error) -> ()) {
+        user.signUpInBackground { (succeeded: Bool, error: Error?) in
+            // Do something!
+            if let error = error {
+                failure(error)
+            } else {
+                success()
+            }
+        }
+    }
     
 }
--- ParseApp/ParseLoginViewController.swift
+++ ParseApp/ParseLoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class ParseLoginViewController: UIViewController {
     
@@ -35,10 +36,68 @@ class ParseLoginViewController: UIViewController {
     }
     
     @IBAction func onSignUpTapped(_ sender: Any) {
+        self.emailTextField.endEditing(true)
+        self.passwordTextField.endEditing(true)
+        guard self.emailTextField.text != "",
+            self.passwordTextField.text != "" else {
+                self.showErrorAlert(withMessage: "The email and/or the password fields cannot be blank.", andTitle: "Input Error")
+                return
+        }
+        
+        let email: String = self.emailTextField.text!
+        let username: String = email
+        let password: String = self.passwordTextField.text!
+        let parseClient: ParseClient = ParseClient.sharedInstance
+        
+        let user: PFUser = parseClient.newUser(username: username, email: email, password: password)
+        parseClient.signUp(user: user, success: {
+            // Do something
+            print("I've signed up!")
+            parseClient.login(user: user, success: { (user: PFUser) in
+                // Do something with a, now, logged in user.
+                print("I've logged in!")
+            }, failure: { (error: Error) in
+                self.showErrorAlert(withMessage: error.localizedDescription, andTitle: "Parse Error")
+            })
+        }) { (error: Error) in
+            // Do something on error
+            self.showErrorAlert(withMessage: error.localizedDescription, andTitle: "Parse Error")
+        }
     }
 
     @IBAction func onLoginTapped(_ sender: Any) {
+        self.emailTextField.endEditing(true)
+        self.passwordTextField.endEditing(true)
+        guard self.emailTextField.text != "",
+            self.passwordTextField.text != "" else {
+                self.showErrorAlert(withMessage: "The email and/or the password fields cannot be blank.", andTitle: "Input Error")
+                return
+        }
+        
+        let email: String = self.emailTextField.text!
+        let username: String = email
+        let password: String = self.passwordTextField.text!
+        let parseClient: ParseClient = ParseClient.sharedInstance
+        
+        let user: PFUser = parseClient.newUser(username: username, email: email, password: password)
+        parseClient.login(user: user, success: { (loggedInUser: PFUser) in
+            print("I've logged in!")
+        }) { (error: Error) in
+            self.showErrorAlert(withMessage: error.localizedDescription, andTitle: "Parse Error")
+        }
     }
+    
+    
+    // MARK: - Logistics
+    
+    private func showErrorAlert(withMessage: String, andTitle title: String) {
+        let alertController: UIAlertController = UIAlertController(title: title, message: withMessage, preferredStyle: .alert)
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
